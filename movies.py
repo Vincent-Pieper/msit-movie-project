4ad08f4410590e101d9753275e21de1d2002d6a1
Vincent-Pieper <pieper.vincent@gmail.com>
2026-06-30 23:35:59 +0200

    Add fuzzy search, colors, and histogram export

--- movies.py
+++ movies.py
@@ -1,5 +1,7 @@
 import random
 import matplotlib.pyplot as plt
+from colorama import Fore, init
+init(autoreset=True)
 
 
 def create_database():
@@ -19,7 +21,7 @@ def create_database():
 
 
 def choose_from_main_menu():
-    print(
+    print(Fore.GREEN +
         "Menu:\n"
         "1. List movies\n"
         "2. Add movie\n"
@@ -31,7 +33,7 @@ def choose_from_main_menu():
         "8. Movies sorted by rating\n"
         "9. Create Rating Histogram\n"
     )
-    return input("Enter choice (1-9): ")
+    return input(Fore.LIGHTGREEN_EX + "Enter choice (1-9): ")
 
 
 def forwarding_main_menu(main_menu_choice, movies):
@@ -54,7 +56,7 @@ def forwarding_main_menu(main_menu_choice, movies):
     elif main_menu_choice == "9":
         rating_histogram(movies)
     else:
-        print(
+        print(Fore.RED +
             "\n"
             "Please make sure your answer matches the numbers displayed in the main menu\n"
             "...Returning to main menu..."
@@ -64,41 +66,41 @@ def forwarding_main_menu(main_menu_choice, movies):
 
 def list_movies(movies):
     print()
-    print(f"{len(movies)} movies in total")
+    print(Fore.CYAN + f"{len(movies)} movies in total")
     for movie, rating in movies.items():
-        print(f"{movie}: {rating}")
+        print(Fore.CYAN + f"{movie}: {rating}")
     returning_to_main()
 
 
 def add_movie(movies):
     print()
-    new_movie_title = input("Enter new movie name: ")
-    new_movie_rating = float(input("Enter new movie rating (0-10): "))
+    new_movie_title = input(Fore.LIGHTGREEN_EX + "Enter new movie name: ")
+    new_movie_rating = float(input(Fore.LIGHTGREEN_EX + "Enter new movie rating (0-10): "))
     movies[new_movie_title] = new_movie_rating
-    print(f"Movie {new_movie_title} successfully added")
+    print(Fore.CYAN + f"Movie {new_movie_title} successfully added")
     returning_to_main()
 
 
 def delete_movie(movies):
     print()
-    deletion_choice = input("Enter movie name to delete: ")
+    deletion_choice = input(Fore.LIGHTGREEN_EX + "Enter movie name to delete: ")
     if deletion_choice in movies:
         del movies[deletion_choice]
-        print(f"Movie {deletion_choice} successfully deleted")
+        print(Fore.CYAN + f"Movie {deletion_choice} successfully deleted")
     else:
-        print(f"Movie {deletion_choice} doesn't exist!")
+        print(Fore.RED + f"Movie {deletion_choice} doesn't exist!")
     returning_to_main()
 
 
 def update_movie(movies):
     print()
-    update_choice = input("Enter movie name to update: ")
+    update_choice = input(Fore.LIGHTGREEN_EX + "Enter movie name to update: ")
     if update_choice in movies:
-        update_movie_rating =  float(input("Enter new movie rating (0-10): "))
+        update_movie_rating =  float(input(Fore.LIGHTGREEN_EX + "Enter new movie rating (0-10): "))
         movies[update_choice] = update_movie_rating
-        print(f"Movie {update_choice} successfully updated")
+        print(Fore.CYAN + f"Movie {update_choice} successfully updated")
     else:
-        print(f"Movie {update_choice} doesn't exist!")
+        print(Fore.RED + f"Movie {update_choice} doesn't exist!")
     returning_to_main()
 
 
@@ -150,40 +152,126 @@ def worst_movie_rating(movies):
 
 
 def printing_stats(average_rating, median_rating, best_movies, worst_movies):
-    print(f"Average rating: {average_rating:.2f}")
-    print(f"Median rating: {median_rating:.2f}")
+    print(Fore.CYAN + f"Average rating: {average_rating:.2f}")
+    print(Fore.CYAN + f"Median rating: {median_rating:.2f}")
     if len(best_movies) > 1:
-        print(f"{len(best_movies)} Movies share the category best movie with a rating of {best_movies[0][1]}")
+        print(Fore.CYAN + f"{len(best_movies)} Movies share the category best movie with a rating of {best_movies[0][1]}")
         for movie in best_movies:
-            print(f"- {movie[0]}")
+            print(Fore.CYAN + f"- {movie[0]}")
     else:
-        print(f"Best movie: {best_movies[0][0]}, {best_movies[0][1]}")
+        print(Fore.CYAN + f"Best movie: {best_movies[0][0]}, {best_movies[0][1]}")
     if len(worst_movies) > 1:
-        print(f"{len(worst_movies)} Movies share the category worst movie with a rating of {worst_movies[0][1]}")
+        print(Fore.CYAN + f"{len(worst_movies)} Movies share the category worst movie with a rating of {worst_movies[0][1]}")
         for movie in worst_movies:
-            print(f"- {movie[0]}")
+            print(Fore.CYAN + f"- {movie[0]}")
     else:
-        print(f"Worst movie: {worst_movies[0][0]}, {worst_movies[0][1]}")
+        print(Fore.CYAN + f"Worst movie: {worst_movies[0][0]}, {worst_movies[0][1]}")
 
 
 def random_movie(movies):
     print()
     r_movie, r_rating = random.choice(list(movies.items()))
-    print(f"Your movie for tonight: {r_movie}, it's rated {r_rating}")
+    print(Fore.CYAN + f"Your movie for tonight: {r_movie}, it's rated {r_rating}")
     returning_to_main()
 
 
 def search_movie(movies):
     print()
-    search_input = input("Enter part of movie name: ").lower()
+    search_word = input(Fore.LIGHTGREEN_EX + "Enter part of movie name: ").lower()
     counter = 0
     for movie, rating in movies.items():
-        if search_input in movie.lower():
-            print(f"{movie}: {rating}")
+        if search_word in movie.lower():
+            print(Fore.CYAN + f"{movie}: {rating}")
             counter += 1
     if counter == 0:
-        print(f"No movie containing '{search_input}' found.")
+        print()
+        fuzzy_search(search_word, movies)
     returning_to_main()
+
+
+def fuzzy_search(search_word,movies ):
+    fuzzy_hits = []
+    max_errors = max_deviation_error_value(search_word)
+    for compare_word_normal in movies: # checks every film in the database movies
+        compare_words = compare_word_normal.lower().split() # splits into single words for comparing shorter entries
+        compare_title = compare_word_normal.lower()
+        found_match = False
+        for word in compare_words: #  splits the compare_word/title into words and compares against them, better chance for short inputs / single words
+            #  print(word)
+            edit_distance = calculate_edit_distance(search_word, word)
+            if edit_distance <= max_errors:
+                fuzzy_hits.append(compare_word_normal)
+                found_match = True
+                break
+        if found_match == False:
+            edit_distance = calculate_edit_distance(search_word, compare_title) #  compares against the full title, better chance for longer inputs
+            if edit_distance <= max_errors:
+                fuzzy_hits.append(compare_word_normal)
+    if len(fuzzy_hits) > 0:
+        print(Fore.YELLOW + f"The movie '{search_word}' does not exist. Did you mean:")
+        for hit in fuzzy_hits:
+            print(Fore.YELLOW + f"{hit}: {movies[hit]}")
+    else:
+        print(Fore.RED + f"No movie containing '{search_word}' found.")
+
+
+def max_deviation_error_value(search_word):
+    if len(search_word) <= 2: # makes sure Star Wars Episode 'x' doesn't match a single input
+        max_errors = 0
+    elif len(search_word) <= 4:  # creates adaptive leniency for spelling errors.
+        max_errors = 1
+    elif len(search_word) <= 7: # 'gdfthr' now doesn't return half the movie base
+        max_errors = 3
+    elif len(search_word) <= 10: # "gudfatha" "drk knight" works
+        max_errors = 4
+    elif len(search_word) <= 15: # 'pulp fiktion' doesnt hit anything...implementing against the whole title again...
+        max_errors = 5
+    else:
+        max_errors = 6
+    return max_errors
+
+def calculate_edit_distance(search_word, compare_entity):
+    matrix_basic, rows, columns = build_empty_fuzzy_matrix(search_word, compare_entity)
+    matrix_universal_rows = fill_first_row_column_fuzzy_matrix(matrix_basic, rows, columns)
+    edit_distance = fill_fuzzy_matrix(matrix_universal_rows, rows, columns, search_word, compare_entity)
+    return edit_distance
+
+
+def build_empty_fuzzy_matrix(search_word = "test", compare_word = "tast"):
+    matrix = []
+    rows = len(search_word) + 1
+    columns = len(compare_word) + 1
+    for row in range(rows):
+        new_row = [] # every row (len(search_word) + 1) creates an empty list
+        for column in range(columns):
+            new_row.append(0) # for every column (len(compare_word) + 1) in the matrix it gets a default value of 0
+        matrix.append(new_row) #  the new row get added to the matrix as an additional list (each list is a row with as many default values as there are columns) - empty body is build
+    return matrix, rows, columns
+
+
+def fill_first_row_column_fuzzy_matrix(matrix, rows, columns):
+    for row in range(rows):
+        for column in range(columns):
+            if row == 0 and column == 0: #  top left is always 0 because it compares two empty strings
+                matrix[row][column] = 0
+            elif row == 0 and column != 0: #  first row gets the value to the left +1
+                matrix[row][column] = matrix[row][column - 1] + 1
+            elif row != 0 and column == 0: #  first column gets the value above +1
+                matrix[row][column] = matrix[row - 1][column] + 1
+    return matrix
+
+
+def fill_fuzzy_matrix(matrix, rows, columns, search_word, compare_word):
+    for row in range(1, rows):
+        for column in range(1, columns):
+            if search_word[row -1] == compare_word[column -1]: #  if the same index of both words is the same take the diagonal value as diag / -1 for the first empty column/row
+                diag = matrix[row - 1][column - 1]
+            elif search_word[row -1] != compare_word[column -1]: #  if the same index of both words is NOT the same take the diagonal value +1 as diag / -1 for the first empty column/row
+                diag = matrix[row - 1][column - 1] +1
+            left = matrix[row][column -1] +1 #  takes the value to the left and adds +1
+            top = matrix[row -1][column] +1 #  takes the value to the top and adds +1
+            matrix[row][column] = min(diag, left, top) # uses the smallest value to fill the cell
+    return matrix[-1][-1]
 
 
 def movies_sorted_ratings(movies):
@@ -199,30 +287,33 @@ def movies_sorted_ratings(movies):
         del remaining_movies[highest_rated_movie[0]]
         highest_rated_movie = ("x",-1)
     for film, rating in sorted_movies:
-        print(f"{film}: {rating}")
+        print(Fore.CYAN + f"{film}: {rating}")
     returning_to_main()
 
 
 def rating_histogram(movies):
     print()
     movie_values = list(movies.values())
-    plt.hist(movie_values, bins = max(5, min(len(movies) // 2 + 5, 50)))
+    file_name = input(Fore.LIGHTGREEN_EX + "Enter file name or path to save the histogram: ")
+    plt.hist(movie_values, bins = max(5, min(len(movies) // 2 + 5, 50))) # number of columns get tied to the number of films
     plt.xlabel("Rating")
     plt.ylabel("Number of movies")
     plt.title("Distribution of movie ratings")
+    plt.savefig(file_name)
     plt.show()
+    plt.close()
     returning_to_main()
 
 
 def returning_to_main():
     print()
-    input("Press enter to continue")
+    input(Fore.LIGHTGREEN_EX + "Press enter to continue")
     print()
 
 
 def main():
     movies = create_database()
-    print("********** My Movies Database **********")
+    print(Fore.LIGHTMAGENTA_EX + "********** My Movies Database **********")
     while True:
         main_menu_choice = choose_from_main_menu()
         forwarding_main_menu(main_menu_choice, movies)
